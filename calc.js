74556f2114d4ca71d9dd6643edc7251e889bf274
Obinna Ezeani <105417409+Emajero@users.noreply.github.com>
2022-06-24 18:04:42 +0100

    Updated calc.js
    Updated the calc.js code to allow the calculator to alert my result.

--- calc.js
+++ calc.js
@@ -7,19 +7,23 @@ var operator = prompt('Enter operator (either +, -, * or /): ');
 var num1 = parseFloat(prompt('Enter first number: '));
 var num2 = parseFloat(prompt('Enter second number: '));
 
-let result;
+let result = 0;
 
 // using if...else if...else
-if (operator == '+'){
-    result = num1 + num2
+if (isNaN(num1) || isNaN(num2)){
+    alert('Wrong Input! Please provide data');
+}else{
+    if (operator == '+'){
+        result = num1 + num2
+    }
+    else if(operator == '-'){
+        result = num1 - num2
+    }
+    else if(operator == '*'){
+        result = num1 * num2
+    }
+    else (operator == '/');{
+        result = num1 / num2
+    }
+    alert(num1 + operator + num2 + ' = ' + result)
 }
-else if(operator == '-'){
-    result = num1 - num2
-}
-else if(operator == '*'){
-    result = num1 * num2
-}
-else (operator == '/');{
-    result = num1 / num2
-}
-alert('${num1} ${operator} ${num2} = ${result}');
